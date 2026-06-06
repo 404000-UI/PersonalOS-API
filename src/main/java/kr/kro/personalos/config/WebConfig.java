88ba3279c209e48ec5586c931c5daa2f587e56c0
404000-UI <pingkingscissors6@gmail.com>
2026-06-06 23:32:30 +0900

    Check CORS Setting

--- src/main/java/kr/kro/personalos/config/WebConfig.java
+++ src/main/java/kr/kro/personalos/config/WebConfig.java
@@ -7,8 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    public WebConfig() {
+        System.out.println("===== WebConfig Loaded =====");
+    }
+    
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        System.out.println("===== CORS Applied =====");
+        
         registry.addMapping("/**").allowedOriginPatterns("*").allowedMethods("*").allowedHeaders("*");
     }
 
